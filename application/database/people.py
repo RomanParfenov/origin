a39b7794246448bfe684000c2f0a8464354e4b75
Roman Parfenov <lotus4@mail.ru>
2021-11-21 21:49:36 +0400

    Lesson3

--- application/database/people.py
+++
@@ -1,8 +0,0 @@
-
-def name_employee():
-    name = input("Введите имя сотрудника \n")
-    department = input("Введите имя отдела \n")
-    salary = float(input('Введите величину зарплаты \n'))
-    return [salary, department, name]
-
-data_employee = name_employee()
