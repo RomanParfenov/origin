8a16ef4ea831e112afeaf62cd8556a0932f0284b
Roman Parfenov <lotus4@mail.ru>
2021-11-29 21:01:02 +0400

    Decorators.Task3

---
+++ Accountans/application/salary.py
@@ -0,0 +1,16 @@
+
+from logger_two import make_logger_two
+
+@make_logger_two
+def calculate_salary(salary, department, name):
+    if(salary > 50000):
+        tax = 0.15*salary
+        netsalary = salary - tax
+        print(f'Заработная плата сотрудника {name} отдела {department} составляет: {netsalary}',
+        f'Налоговые выплаты составляют {tax}', sep="\n")
+
+    else:
+        netsalary = salary
+        print(f'Заработная плата сотрудника {name} отдела {department} составляет: {netsalary}',
+        f'Налог не удерживается', sep="\n")
+    return netsalary
